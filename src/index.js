ddf5b5bf879aaa19dde3cc197c0e66d5d2eb8603
tsotne <tsotnemeskhuri@gmail.com>
2024-08-26 15:14:23 +0400

    Fix typo

--- src/index.js
+++ src/index.js
@@ -124,7 +124,7 @@ function Footer() {
 function Order({ closeHour }) {
   return (
     <div className="order">
-      <p>We're open until {closeHour}:00. Come isit us or order online.</p>
+      <p>We're open until {closeHour}:00. Come visit us or order online.</p>
       <button className="btn">Order</button>
     </div>
   );
