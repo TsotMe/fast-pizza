e1004f2ff324d77b74df9832be521afc48bbfdf9
tsotne <tsotnemeskhuri@gmail.com>
2024-11-21 00:50:23 +0400

    Add some taiwind styles

---
+++ src/ui/Button.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+export default function Button({ disabled, children, to, type }) {
+  const base =
+    "inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+
+  const styles = {
+    primary: base + " px-4 py-3 md:px-6 md:py-4",
+    small: base + " px-2 py-1 md:px-3 md:py-2 text-xs",
+  };
+
+  if (to) {
+    return (
+      <Link className={styles[type]} to={to}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button disabled={disabled} className={styles[type]}>
+      {children}
+    </button>
+  );
+}
