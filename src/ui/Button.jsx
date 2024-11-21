22d2fd135252fbe864c054e9c52b9ba0e9a55e05
tsotne <tsotnemeskhuri@gmail.com>
2024-11-21 15:54:21 +0400

    Add onClick prop on Button ui component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export default function Button({ disabled, children, to, type }) {
+export default function Button({ disabled, children, to, type, onClick }) {
   const base =
     "text-sm inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
@@ -16,6 +16,14 @@ export default function Button({ disabled, children, to, type }) {
       <Link className={styles[type]} to={to}>
         {children}
       </Link>
+    );
+  }
+
+  if (onClick !== undefined) {
+    return (
+      <button disabled={disabled} className={styles[type]} onClick={onClick}>
+        {children}
+      </button>
     );
   }
 
