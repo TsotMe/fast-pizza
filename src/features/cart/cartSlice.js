0f3caaa9c2c870409061fc30933ad61b39d8d29a
tsotne <tsotnemeskhuri@gmail.com>
2024-11-21 16:44:15 +0400

    Add getCurrentQuantityById selector function in cartSlice

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -54,3 +54,6 @@ export const getTotalPizzaQuantity = (state) =>
 
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (pizzaId) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === pizzaId)?.quantity ?? 0;
